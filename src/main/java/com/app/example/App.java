2bfb0d665f1ab475a9cbeca10797aa8e01f96b09
Suresh-kilari <92583967+Suresh-kilari@users.noreply.github.com>
2021-12-07 18:56:25 +0530

    Update App.java

--- src/main/java/com/app/example/App.java
+++ src/main/java/com/app/example/App.java
@@ -1,13 +1,25 @@
 package com.app.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App extends SpringBootServletInitializer {
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(App.class);
+   }
+   public static void main(String[] args) {
+      SpringApplication.run(App.class, args);
+   }
+
+   @RequestMapping(value = "/")
+   public String hello() {
+      return "<center>You have successfully completed Maven with Tomcat Project</center>";
+   }
 }
